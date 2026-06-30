2954be0c019756460ccec8e8804ca83ba4e0836d
IshKevin <carterk279@gmail.com>
2026-06-30 14:19:06 +0200

    Refactor jest.config.mjs to remove unnecessary line breaks in collectCoverageFrom

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,16 +6,12 @@ const createJestConfig = nextJest({ dir: "./" });
 const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  
+
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/**/*.d.ts",
-    "!src/**/index.ts",
-  ],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/**/index.ts"],
 };
 
 export default createJestConfig(config);
